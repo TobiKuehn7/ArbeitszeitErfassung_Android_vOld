bf0f992527804cdc2c7049c9463e004ca3fba494
Tobias Kühn <tobias.kuehn@ges.thm.de>
2021-05-29 17:16:11 +0200

    added a new date time test

--- app/src/test/java/de/tkapps/arbeitszeiterfassung/DateAndTimeHelpersTest.java
+++ app/src/test/java/de/tkapps/arbeitszeiterfassung/DateAndTimeHelpersTest.java
@@ -12,6 +12,18 @@ import static org.junit.Assert.assertEquals;
 public class DateAndTimeHelpersTest {
     @Test
     public void makeDateTime_isCorrect () {
+        String expected_result = "Thu May 20 15:05:23 CEST 2021";
+
+        String strTime = "15:05:23";
+        String strDate = "20.05.2021";
+
+        Date actual_result = timeHelpers.makeDateTime(strDate, strTime);
+
+        assertEquals(expected_result, actual_result.toString());
+    }
+
+    @Test
+    public void makeDateTimeAndConvertToString_isCorrect () {
 
         String expected_result = "20.05.2021 15:05:23";
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd.MM.yyyy HH:mm:ss");
