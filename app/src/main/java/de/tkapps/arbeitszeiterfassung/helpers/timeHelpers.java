5077a2b3702ccfeec1a1323e809e56167fc71d4d
Tobias Kühn <tobias.kuehn@ges.thm.de>
2021-10-07 22:38:56 +0200

    added new calculation and changed UI to fit the new time calculations

--- app/src/main/java/de/tkapps/arbeitszeiterfassung/helpers/timeHelpers.java
+++ app/src/main/java/de/tkapps/arbeitszeiterfassung/helpers/timeHelpers.java
@@ -1,137 +1,60 @@
 package de.tkapps.arbeitszeiterfassung.helpers;
 
+
 import java.text.DateFormat;
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.Date;
-import java.util.Locale;
-import java.util.TimeZone;
 
 public class timeHelpers {
 
     /**
-     * method to get date and convert to string
-     * @return date as string
+     * this function converts a string which contains a datetime to a date which contains a datetime
+     * @param str the string should contain a datetime in this format: "YYYY-MM-DD'T'HH:mm:ss" (example: "2021-10-07T14:06:54")
+     * @return a Date which contains the given datetime from the string
      */
-    public static String getDate() {
-
-        // make a formatter that can parse the date
-        String pattern = "dd.MM.yyyy";
-        SimpleDateFormat formatter= new SimpleDateFormat(pattern, Locale.GERMAN);
-
-        // get datetime
-        Date date = new Date(System.currentTimeMillis());
-
-        // return formatted date
-        return formatter.format(date);
+    public static Date saveStringToDate(String str) {
+        LocalDateTime dateTime = LocalDateTime.parse(str);
+        return java.util.Date
+                .from(dateTime.atZone(ZoneId.systemDefault())
+                        .toInstant());
     }
 
     /**
-     * method to get time and convert to string
-     * @return time as string
+     * this function function turns a show string into a date object
+     * @param str the show string of the date
+     * @return a Date which contains the given datetime from the string
      */
-    public static String getTime() {
+    public static Date showStringToDate(String str) {
+        String[] dateTimeParts = str.split(": ")[1].split(", ");
 
-        // make a formatter that can parse the date
-        String pattern = "HH:mm:ss";
-        SimpleDateFormat formatter= new SimpleDateFormat(pattern, Locale.GERMAN);
+        String[] dateParts = dateTimeParts[0].split("\\.");
+        String date = dateParts[2] + "-" + dateParts[1] + "-" + dateParts[0];
 
-        // get datetime
-        Date time = new Date(System.currentTimeMillis());
+        String time = dateTimeParts[1];
 
-        // return formatted time
-        return formatter.format(time);
+        return saveStringToDate(date + "T" + time);
     }
 
     /**
-     * method to convert a datetime string into a date object
-     * @param date string that contains a date in format "dd.MM.yyyy"
-     * @param time string that contains a time in format "HH:mm:ss"
-     * @return returns the date object of the datetime strings passed in
+     * this function converts a date object which contains a datetime to a string into this format: "yyyy-MM-dd'T'HH:mm:ss"
+     * @param date the date object which should be converted to string
+     * @return the string which can be saved
      */
-    public static Date makeDateTime(String date, String time) {
-
-        // make new Date object which will be returned
-        Date dateTime = new Date();
-
-        // make a formatted datetime string from input
-        String strDateTime = date + " " + time;
-
-        // make a formatter that can parse that date
-        String pattern = "dd.MM.yyyy HH:mm:ss";
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern, Locale.GERMAN);
-
-        // parse datetime
-        try {
-            dateTime = simpleDateFormat.parse(strDateTime);
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-
-        // return date object of passed strings
-        return dateTime;
+    public static String dateToSaveString(Date date) {
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
+        return dateFormat.format(date);
     }
 
     /**
-     * methode to calculate the netto work time and the total pause time
-     * @param dateDifference difference between work start and work end
-     * @param datePause1Start datetime object of pause 1 beginning
-     * @param datePause1End datetime object of pause 1 ending
-     * @param datePause2Start datetime object of pause 2 beginning
-     * @param datePause2End datetime object of pause 2 ending
-     * @return the pause time and the netto working time
+     * this function is for returning a better string from given date for showing in app
+     * @param date the date object which should be converted to string
+     * @return the string which can be shown in app
      */
-    public static Date[] calcTimeDiffWithPauses(Date dateDifference, Date datePause1Start, Date datePause1End, Date datePause2Start, Date datePause2End) {
-        // prepare array for returning
-        Date[] datesWithPauses = new Date[2];
-
-        // date 1 for array
-        long pause1Diff = datePause1End.getTime() - datePause1Start.getTime();
-
-        Date arbeitszeitNetto;
-        Date pausenzeit;
-        String pausenZeitFormatted;
-
-        if (!datePause2Start.equals(new Date(0))) {
-
-            // date 2 for array
-            long pause2Diff = datePause2End.getTime() - datePause2Start.getTime();
-
-            // pause time total
-            long pauseTotal = pause1Diff + pause2Diff;
-            pausenzeit = new Date(pauseTotal);
-
-            DateFormat formatter = new SimpleDateFormat("HH:mm:ss");
-            formatter.setTimeZone(TimeZone.getTimeZone("MEZ"));
-            pausenZeitFormatted = formatter.format(pausenzeit);
-
-            // work time netto
-            long arbeitszeit = dateDifference.getTime() - pausenzeit.getTime();
-            arbeitszeitNetto = new Date(arbeitszeit);
-
-
-        } else {
-
-            // pause time total
-            long pauseTotal = pause1Diff;
-            pausenzeit = new Date(pauseTotal);
-            DateFormat formatter = new SimpleDateFormat("HH:mm:ss");
-            formatter.setTimeZone(TimeZone.getTimeZone("MEZ"));
-            pausenZeitFormatted = formatter.format(pausenzeit);
-
-            // work time netto
-            long arbeitszeit = dateDifference.getTime() - pausenzeit.getTime();
-            arbeitszeitNetto = new Date(arbeitszeit);
-
-        }
-
-        // set dates into array
-        datesWithPauses[0] = arbeitszeitNetto;
-        datesWithPauses[1] = pausenzeit;
-
-
-        // return array
-        return datesWithPauses;
+    public static String dateToShowString(Date date) {
+        DateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy', 'HH:mm:ss");
+        return dateFormat.format(date);
     }
 
     /**
@@ -142,8 +65,7 @@ public class timeHelpers {
      */
     public static Date calcTimeDiff(Date dateStart, Date dateEnd) {
         long timeDifference = dateEnd.getTime() - dateStart.getTime();
-        Date diff = new Date(timeDifference);
 
-        return diff;
+        return new Date(timeDifference);
     }
 }
