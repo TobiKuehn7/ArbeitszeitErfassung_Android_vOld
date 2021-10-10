f38eef8ceac1c7738abc2836a6cf43f81a363cdb
Tobias Kühn <tobias.kuehn@ges.thm.de>
2021-10-10 23:01:46 +0200

    changed gradle versions and other stuff following stackoverflow (https://stackoverflow.com/questions/44429751/how-to-use-junit-5-with-gradle)

--- app/src/test/java/de/tkapps/arbeitszeiterfassung/TimeHelpersUnitTest.java
+++ app/src/test/java/de/tkapps/arbeitszeiterfassung/TimeHelpersUnitTest.java
@@ -1,9 +1,9 @@
 package de.tkapps.arbeitszeiterfassung;
 
-import org.junit.Test;
 import java.util.Date;
 import de.tkapps.arbeitszeiterfassung.helpers.TimeHelpers;
-import static org.junit.Assert.assertEquals;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class TimeHelpersUnitTest {
